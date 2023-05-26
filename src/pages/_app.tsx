03fe607139593874541169753411639bd63dffba
Noahdcoder <noahnosaaghedo@gmail.com>
2023-05-26 14:39:28 +0100

    Fixed double scrollbar during page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence, motion } from "framer-motion";
-import "@fortawesome/fontawesome-svg-core/styles.css"; //importing font awesome css
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    const handleRouteChange = () => {
+      document.body.style.overflow = "hidden";
+    };
+
+    const handleRouteChangeComplete = () => {
+      document.body.style.overflow = "visible";
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeComplete);
+    };
+  }, [router]);
+
   return (
     <AnimatePresence mode="wait">
       <motion.div
@@ -14,6 +35,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
         transition={{ duration: 0.5 }}
+        className="overflow-hidden"
       >
         <Component {...pageProps} className="font-nunito" />
       </motion.div>
